3e47f606b4016f70a98ba5f7050be817a8342d88
manikchand1995 <manikchand.p@zohocorp.com>
2020-05-24 16:07:52 +0530

    firebase apis in local firebase class

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCbEBL1RfebwXcQy2vHw3MeM-7g2gN-vng",
@@ -14,7 +15,22 @@ const firebaseConfig = {
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
+    this.auth = app.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+ 
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
+
 }
  
 export default Firebase;
