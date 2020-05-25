9d05fe541b2d1ec5af607d998280beb0644e8a42
manikchand1995 <manikchand.p@zohocorp.com>
2020-05-25 11:33:55 +0530

    complete auth

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
  
 import PasswordChangeForm from '../PasswordChange';
- 
+import { AuthUserContext, withAuthorization } from '../Session';
+
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
  
-export default AccountPage;
+export default withAuthorization(authUser => !!authUser)(AccountPage);
